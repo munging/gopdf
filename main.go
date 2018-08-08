64fa8c564ee6cc11c8a6c9e9e878be12610bc3d2
John Bampton <jbampton@users.noreply.github.com>
2018-08-08 18:18:48 +1000

    Add files

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"log"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	})
+	router.Run(":" + port)
+}
